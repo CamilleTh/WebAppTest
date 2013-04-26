9e9d021dd517de42a9c138bca8a5abe0baa62628
camille <camille@camille-550P5C-550P7C.(none)>
2013-04-26 11:49:46 +0200

    first commit

---
+++ src/lu/intech/servlets/Test.java
@@ -0,0 +1,23 @@
+package lu.intech.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.Request;
+
+public class Test extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String message = "My awesome message";
+		req.setAttribute("test", message);
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/jsp/test.jsp" ).forward( req, resp );
+	}
+
+}
